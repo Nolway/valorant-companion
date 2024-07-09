0bfc3ba6d2448202df35192ad3496e59552a9933
Alexis <alexis.faizeau@animedigitalnetwork.fr>
2024-07-09 12:57:22 +0200

    fix: fix translations

---
+++ src/i18n/en-US/authentication/index.ts
@@ -0,0 +1,9 @@
+import type { BaseTranslation } from '../../i18n-types'
+
+const en_US_authentication = {
+    logout: {
+        reply: 'You have been logged out.',
+    },
+} satisfies BaseTranslation;
+
+export default en_US_authentication;
---
+++ src/i18n/fr/authentication/index.ts
@@ -0,0 +1,9 @@
+import type { NamespaceAuthenticationTranslation } from '../../i18n-types'
+
+const fr_authentication = {
+  logout: {
+    reply: 'You have been logged out.',
+  },
+} satisfies NamespaceAuthenticationTranslation;
+
+export default fr_authentication;
