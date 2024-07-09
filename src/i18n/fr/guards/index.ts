08957817325ffc36942bb10c39beda44ea83c97f
Alexis <alexis.faizeau@animedigitalnetwork.fr>
2024-07-09 12:45:59 +0200

    fix: fix translations

--- src/i18n/fr/guards/index.ts
+++ src/i18n/fr/guards/index.ts
@@ -1,10 +1,10 @@
 import type { NamespaceGuardsTranslation } from '../../i18n-types'
 
-const fr_FR_guards = {
+const fr_guards: NamespaceGuardsTranslation = {
   onlyDm: {
     reply: 'Cette commande ne peut être utilisée que dans les messages privés.',
     userDm: "Vous avez essayé d'utiliser la commande `{commandName}` dans le salon d'un serveur. Cette commande ne peut être utilisée que ici.",
   },
-} satisfies NamespaceGuardsTranslation
+};
 
-export default fr_FR_guards
+export default fr_guards;
